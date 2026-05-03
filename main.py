6a49c09c090e7486d5933b54c84a3f2cfccdef08
Vasilisa Zaytseva <vasilisazaytseva6454@gmail.com>
2026-05-03 13:21:49 +0300

    Добавила основные функции

--- main.py
+++ main.py
@@ -4,4 +4,25 @@ from tkinter import ttk, messagebox
 import json
 import os
 
+data_file = "books.json"
+books = []
+
+def load_data():
+    pass
+
+def save_data():
+    pass
+
+def display_books(data_to_show=None):
+    pass
+
+def add_book():
+    pass
+
+def filter_books():
+    pass
+
+def reset_filter():
+    pass
+
 
